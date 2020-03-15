6ead17ed724a19aff54156afb063ae68bb0adf2f
Elijah <elijah@invoicesimple.com>
2020-03-14 21:43:57 -0700

    fixed crop

--- android/src/main/java/fr/michaelvilleneuve/customcrop/RNCustomCropModule.java
+++ android/src/main/java/fr/michaelvilleneuve/customcrop/RNCustomCropModule.java
@@ -85,7 +85,7 @@ public class RNCustomCropModule extends ReactContextBaseJavaModule {
     Point bl = new Point(points.getMap("bottomLeft").getDouble("x"), points.getMap("bottomLeft").getDouble("y"));
     Point br = new Point(points.getMap("bottomRight").getDouble("x"), points.getMap("bottomRight").getDouble("y"));
 
-    Mat src = Imgcodecs.imread(imageUri.replace("file://", ""), Imgproc.COLOR_BGR2RGB);
+    Mat src = Imgcodecs.imread(imageUri.replace("file://", ""));
     Imgproc.cvtColor(src, src, Imgproc.COLOR_BGR2RGB);
 
     boolean ratioAlreadyApplied = false; //tr.x * (src.size().width / 500) < src.size().width;
